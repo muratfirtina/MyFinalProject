513670d686af2c24ba024cf4bb34a214c2ba82c9
murat firtina <muratfirtina@hotmail.com>
2021-02-28 02:02:08 +0300

    Add project files.

---
+++ DateAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DateAccess.Abstract
+{
+    //generic constraint
+    //burada class demek referans tip olabilir demek
+    //burada IEntity eklemek IEntity olabilir veya bunu implemente eden bir nesne olabilir.
+    //burada new() demek newlenebilir olmalı demek.
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DateAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using DateAccess.Abstract;
+using Entities.Concrete;
+
+namespace DateAccess.Concrete.EntityFramework
+{
+    public class EfCategoryDal : ICategoryDal
+    {
+        public void add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category Get(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
