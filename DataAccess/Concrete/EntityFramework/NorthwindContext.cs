610b537857e8f949e9f5b44d4e8c1ff473d8032b
murat firtina <muratfirtina@hotmail.com>
2021-03-04 11:56:58 +0300

    9.Gün

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -18,5 +18,6 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Product> Products { get; set; } //hangi veritabanı hangi class a karşılık geliyor
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
